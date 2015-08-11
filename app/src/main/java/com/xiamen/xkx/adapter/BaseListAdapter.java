58976083b1afe2efaf9c2708704827516a64a75c
yuhuazhu <yuhuazhu@live.com>
2015-08-11 17:05:27 +0800

    some utils

---
+++ app/src/main/java/com/xiamen/xkx/adapter/BaseListAdapter.java
@@ -0,0 +1,15 @@
+package com.xiamen.xkx.adapter;
+
+import android.widget.BaseAdapter;
+
+public abstract class BaseListAdapter extends BaseAdapter {
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+}
