37e8ecc1ba2daca5a7e375100eb6538aa3b76c98
boriszixue <1187976965@qq.com>
2015-08-12 14:45:35 +0800

    bluetooth and audio service

---
+++ app/src/main/java/com/xiamen/xkx/service/AudioService.java
@@ -0,0 +1,126 @@
+package com.xiamen.xkx.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.io.IOException;
+
+public class AudioService extends Service {
+    AudioBinder binder = new AudioBinder();
+    private MediaPlayer mediaPlayer;
+    // 是不是已经导入了资源
+    private boolean isFrist;
+    private OnPlayCompleteListener listener;
+
+    public AudioService() {
+    }
+
+    @Override
+    public void onCreate() {
+        mediaPlayer = new MediaPlayer();
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        mediaPlayer.release();
+        return super.onUnbind(intent);
+    }
+
+    public void play(Uri uri) {
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(this, uri);
+            mediaPlayer.setVolume(1, 1);
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
+
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mediaPlayer.start();
+                    isFrist = true;
+                }
+            });
+        } catch (IllegalArgumentException e) {
+            //CrashHandler.getInstance().logStringToFile(e.toString());
+        } catch (SecurityException e) {
+            //CrashHandler.getInstance().logStringToFile(e.toString());
+        } catch (IllegalStateException e) {
+            //CrashHandler.getInstance().logStringToFile(e.toString());
+        } catch (IOException e) {
+            //CrashHandler.getInstance().logStringToFile(e.toString());
+        }
+        mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                listener.onPlayComplete();
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public interface OnPlayCompleteListener {
+        public void onPlayComplete();
+    }
+
+    public class AudioBinder extends Binder {
+        public void setOnPlayCompleteListener(OnPlayCompleteListener lis) {
+            listener = lis;
+        }
+
+        // 初始化并播放
+        public void audioPlay(Uri uri) {
+            play(uri);
+        }
+
+        // 开始播放
+        public void audioStart() {
+            mediaPlayer.start();
+        }
+
+        // 暂停播放
+        public void audioPause() {
+            mediaPlayer.pause();
+            // 设置为true后，暂停服务后不会被kill掉
+            stopForeground(true);
+        }
+
+        // 停止播放
+        public void audioStop() {
+            mediaPlayer.stop();
+        }
+
+        // 是否在播放
+        public boolean audioIsPlaying() {
+            return mediaPlayer.isPlaying();
+        }
+
+        // 是否已经导入资源
+        public boolean audioIsFrist() {
+            return isFrist;
+        }
+
+        // 播放下一个
+        public void audioPlayNext(Uri uri) {
+            isFrist = false;
+            play(uri);
+        }
+    }
+}
