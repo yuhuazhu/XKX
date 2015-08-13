d5451dce5c69fd3766ed08f802ab527d58fbdd2b
boriszixue <1187976965@qq.com>
2015-08-13 11:55:43 +0800

    fix photo display bug

--- app/src/main/java/com/xiamen/xkx/activity/PhotoWashActivity.java
+++ app/src/main/java/com/xiamen/xkx/activity/PhotoWashActivity.java
@@ -13,7 +13,6 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 
 import com.xiamen.xkx.R;
 
@@ -58,6 +57,25 @@ public class PhotoWashActivity extends AppCompatActivity implements OnClickListe
                 startActivity(intent);
                 break;
             case R.id.tv_take_photo:
+                //intent = new Intent(Intent.ACTION_GET_CONTENT);
+                //intent.addCategory(Intent.CATEGORY_OPENABLE);
+                ////这个参数是确定要选择的内容为图片
+                //intent.setType("image/*");
+                ////这个参数 不太懂，唯一知道的是：设置了参数，就会调用裁剪，如果不设置，就会跳过裁剪的过程。
+                //intent.putExtra("crop", "true");
+                ////这个是裁剪时候的 裁剪框的 X 方向的比例。
+                //intent.putExtra("aspectX", 1);
+                ////同上Y 方向的比例. (注意： aspectX, aspectY ，两个值都需要为 整数，如果有一个为浮点数，就会导致比例失效。)
+                //intent.putExtra("aspectY", 1);
+                ////返回数据的时候的 X 像素大小。
+                //intent.putExtra("outputX", 80);
+                ////返回的时候 Y 的像素大小。
+                //intent.putExtra("outputY", 80);
+                ////是否要返回值。 一般都要。
+                //intent.putExtra("return-data", true);
+                //startActivityForResult(intent, 0);
+
+
                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
@@ -115,7 +133,6 @@ public class PhotoWashActivity extends AppCompatActivity implements OnClickListe
                 }
             }
             Intent intent = new Intent();
-
             intent.setClass(this, PhotoTakeActivity.class);
             intent.putExtra("Uri", selectedImage);
             intent.putExtra("fileName", selectedImage.getPath());
