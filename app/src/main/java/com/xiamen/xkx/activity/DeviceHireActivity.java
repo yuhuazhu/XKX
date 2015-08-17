1e4f4aa18449a367b3ce39438aa3fe9557390375
yuhuazhu <yuhuazhu@live,com>
2015-08-17 17:46:56 +0800

    添加设备租用，设备介绍，订单查询Activity

---
+++ app/src/main/java/com/xiamen/xkx/activity/DeviceHireActivity.java
@@ -0,0 +1,18 @@
+package com.xiamen.xkx.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xiamen.xkx.R;
+
+/**
+ * Created by Administrator on 2015/8/17.
+ * 设备租用
+ */
+public class DeviceHireActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devicehire);
+    }
+}
---
+++ app/src/main/java/com/xiamen/xkx/activity/OrderTrackingActivity.java
@@ -0,0 +1,18 @@
+package com.xiamen.xkx.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xiamen.xkx.R;
+
+/**
+ * Created by Administrator on 2015/8/17.
+ * 订单查询
+ */
+public class OrderTrackingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordertracking);
+    }
+}
